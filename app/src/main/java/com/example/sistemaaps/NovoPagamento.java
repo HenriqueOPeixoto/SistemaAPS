9b14358dacc9723008327434d324f2c232261c02
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-07-10 15:37:41 -0400

    Adicionar tela de novo pagamento

---
+++ app/src/main/java/com/example/sistemaaps/NovoPagamento.java
@@ -0,0 +1,15 @@
+package com.example.sistemaaps;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NovoPagamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_pagamento);
+    }
+}
