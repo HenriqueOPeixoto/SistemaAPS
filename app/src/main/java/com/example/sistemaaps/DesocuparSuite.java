2e0745901d6000ac0a816f850ceb8c30108c9cb3
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-07-10 15:36:59 -0400

    Adicionar botão para voltar

--- app/src/main/java/com/example/sistemaaps/DesocuparSuite.java
+++ app/src/main/java/com/example/sistemaaps/DesocuparSuite.java
@@ -2,7 +2,10 @@ package com.example.sistemaaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DesocuparSuite extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class DesocuparSuite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desocupar_suite);
+
+        Button btnNovoPgto = (Button) findViewById(R.id.btnNovoPagamento);
+        btnNovoPgto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DesocuparSuite.this, NovoPagamento.class));
+            }
+        });
+
+        Button btnVoltar = (Button) findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // Voltar para a atividade anterior
+            }
+        });
     }
 }
