a8bbcaa332cce43a6d22c826f7c1a01faf770cf2
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-07-11 15:39:10 -0400

    Animação da tela de processamento

--- app/src/main/java/com/example/sistemaaps/ProcessandoPagamento.java
+++ app/src/main/java/com/example/sistemaaps/ProcessandoPagamento.java
@@ -1,11 +1,14 @@
 package com.example.sistemaaps;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class ProcessandoPagamento extends AppCompatActivity {
 
@@ -27,5 +30,33 @@ public class ProcessandoPagamento extends AppCompatActivity {
                 finish();
             }
         });
+
+        TextView txtProgresso = (TextView) findViewById(R.id.txtProgresso);
+
+        /*
+            As linhas abaixo são reponsáveis pela animação da tela de processamento.
+         */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txtProgresso.setText("Insira seu cartão na máquina\nAguardando Pagamento...");
+            }
+        }, 3000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txtProgresso.setText("Pagamento confirmado!");
+            }
+        }, 8000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 12000);
+
     }
 }
